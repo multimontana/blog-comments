5c3191ad3f241888593a85ed4667fdd3589fa288
Artyom Harutyunyan <artyomharutyunyan@Artyoms-MacBook-Pro.local>
2023-03-26 00:53:36 +0400

    fine app

--- web/src/config/config.php
+++ web/src/config/config.php
@@ -9,7 +9,7 @@ define('URL_ROOT', '/');
 define('URL_SUBFOLDER', '');
 
 //DB Params
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', 'mysql');
 define('DB_USER', 'root');
 define('DB_PASS', 'root');
 define('DB_NAME', 'blog_comments');
